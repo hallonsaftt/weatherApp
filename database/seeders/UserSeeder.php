4a97a110d8d48e6b6543b890f4d42141a93720e8
hallonsaftt <hallonsaftt@gmail.com>
2025-02-27 15:30:08 +0100

    #Seeder 2

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $amount = $this->command->getOutput()->ask("Kolio korisnika zelite da napravite?", 1);
+
+        $faker = \Faker\Factory::create();
+
+//progress bar
+        $this->command->getOutput()->progressStart($amount);
+
+        for($i = 0; $i < $amount; $i++)
+        {
+            User::create([
+
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make('123456'),
+
+            ]);
+
+            $this->command->getOutput()->progressAdvance();
+        }
+
+        $this->command->getOutput()->progressFinish();
+    }
+}
